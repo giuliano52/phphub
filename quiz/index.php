dde98d0673828918159a655a6a1e37efefb2e8f8
Giuliano Dedda <giuliano.dedda@yahoo.com>
2013-11-30 07:22:42 +0100

    modifica quiz

--- quiz/index.php
+++ quiz/index.php
@@ -5,10 +5,12 @@
  *
  *
  */
+error_reporting(E_ALL);
 
- $num_option = 3;
+$NUM_OPTIONS = 3;
  
- function csv_to_array($filename='', $delimiter=';')
+ 
+function csv_to_array($filename='', $delimiter=';')
 {
     if(!file_exists($filename) || !is_readable($filename))
         return FALSE;
@@ -29,15 +31,65 @@
     return $data;
 }
 
+function emit_question($data_quiz,$single_quiz_data) {
 
+	echo '<table border="1">';
+	echo "<tr><td>\n";
+	echo $data_quiz[$single_quiz_data['question_index']]["Nazione"];
+	echo "</td>\n<td>";
+	foreach ($single_quiz_data['answers'] as $key_answer) {
+		
+		echo '<input type="radio" name="q_id'.$single_quiz_data['question_index'].'" value="'.$key_answer.'">';
+		echo $data_quiz[$key_answer]["Capitale"];
+		echo "<br>\n"; 
+		}
+	echo "</td></tr></table>";
 
-$data_quiz=csv_to_array('quiz1.csv');
-foreach ($data_quiz as $single_quiz) {
-	$question = $single_quiz["Nazione"];
-	
-	print $question;
-	
 }
 
+function emit_header() {
+echo '<html>
+<body>
+<form action="result.php" method="get">
+';
+}
+
+function emit_footer() {
+echo '
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
+';
+}
+
+
+// ------------------------- MAIN --------------------
+
+
+emit_header();
+
+$data_quiz=csv_to_array('quiz1.csv');
+shuffle($data_quiz);
+
+
+
+for($question_index = 0; $question_index < count($data_quiz); ++$question_index) {
+	
+	$single_quiz_data['question_index']=$question_index;
+	$single_quiz_data['answers'] = array_rand($data_quiz, $NUM_OPTIONS);
+	
+	shuffle($single_quiz_data['answers']);	
+	if (!in_array($question_index, $single_quiz_data['answers'])) {
+		$single_quiz_data['answers'][0]=$question_index;
+		shuffle($single_quiz_data['answers']);
+		}
+		
+	
+	emit_question($data_quiz,$single_quiz_data);
+
+}
+
+emit_footer();
  
 ?>
---
+++ quiz/result.php
@@ -0,0 +1,13 @@
+<pre>
+<?php
+/* Author: GD
+ * Created: 20131129
+ * Modified: 20131129
+ *
+ *
+ */
+ print_r($_REQUEST)
+ ?>
+ 
+ </pre>
+ 
