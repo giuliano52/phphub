357e7b232cd6e9017e26dbb50a89c5ea24b6e8f7
Giuliano Dedda <giuliano.dedda@yahoo.com>
2013-12-27 19:03:40 +0100

    qualche modifica

--- quiz/quiz_fnc_inc.php
+++ quiz/quiz_fnc_inc.php
@@ -52,7 +52,7 @@ function cmd_inizialize_quiz($quiz_name) {
 	$question_id = 0;
 
 
-	echo "<hr><pre>";print_r($data_quiz_src);echo "</pre>";
+	//echo "<hr><pre>";print_r($data_quiz_src);echo "</pre>";
 	foreach ($data_quiz_src as $single_quiz) {
 		$difficult_level = isset($single_quiz[difficult_level]) ? $single_quiz[difficult_level] : 0 ;
 		if (($difficult_level >= $_SESSION['min_diffucult_level']) && ($difficult_level <= $_SESSION['max_diffucult_level'])) {
@@ -93,7 +93,7 @@ function cmd_inizialize_quiz($quiz_name) {
 	}
 		
 	$_SESSION['data_quiz'] = array_slice($data_quiz,0,$_SESSION['Num_question_total']);
-	echo "<pre>";print_r($_SESSION);echo "</pre>";
+	//echo "<pre>";print_r($_SESSION);echo "</pre>";
 
 	// return $data_quiz;
 }
