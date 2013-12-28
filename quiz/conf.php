ad895e5e7dbba4b653106cc255eac5105580fe5a
Giuliano Dedda <giuliano.dedda@yahoo.com>
2013-12-28 09:56:47 +0100

    quiz: aggiunte bandiere

--- quiz/conf.php
+++ quiz/conf.php
@@ -8,14 +8,14 @@ function initialize_session() {
 	$_SESSION['Num_question_per_page'] = 1 ;  // 0 means no limits
 	$_SESSION['Num_question_total'] = 6 ;  // 0 means all question available
 	$_SESSION['min_diffucult_level'] = 0; // min 0
-	$_SESSION['max_diffucult_level'] = 100; // max 100
+	$_SESSION['max_diffucult_level'] = 1; // max 100
 	$_SESSION['congratulation_file'] = 'conf/congratulation.csv';
 
 
 	
-//	$_SESSION['Quiz_name'] = 'data/quiz_bandiere.csv';
+	$_SESSION['Quiz_name'] = 'data/quiz_bandiere.csv';
 //	$_SESSION['Quiz_name'] = 'data/quiz_capitali.csv';
-	$_SESSION['Quiz_name'] = 'data/quiz_animali.csv';
+//	$_SESSION['Quiz_name'] = 'data/quiz_animali.csv';
 
 
 
